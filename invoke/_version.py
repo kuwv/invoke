75adea8c8d063f62757693355de0d359cb1391e0
Jesse P. Johnson <jpj6652@gmail.com>
2020-12-20 09:39:22 -0500

    ci: disable builds for now

--- invoke/_version.py
+++ invoke/_version.py
@@ -1,4 +1,4 @@
 from importlib_metadata import version  # type: ignore
 
 __version__ = version(__package__)
-__version_info__ = tuple(map(int, __version__.split('.')))
+__version_info__ = tuple(map(int, __version__.split(".")))
